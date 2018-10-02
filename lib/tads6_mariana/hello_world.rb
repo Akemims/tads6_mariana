1eab843a2c10c563454bc2cca9d6166ed0a3e8f7
Akemims <akemims@gmail.com>
2018-10-01 20:01:52 -0400

    modificado metodo say para static

--- lib/tads6_mariana/hello_world.rb
+++ lib/tads6_mariana/hello_world.rb
@@ -7,9 +7,18 @@
 # end
 
 #para passar uma variaval e retornar qualquer palavra
+# module Tads6Mariana
+#   class HelloWorld
+#     def say(word)
+#       word
+#     end
+#   end
+# end
+
+#transforma o metodo em statico
 module Tads6Mariana
   class HelloWorld
-    def say(word)
+    def sefl.say(word)
       word
     end
   end
